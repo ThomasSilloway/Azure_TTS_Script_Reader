e089edc92b071389ae0d876861c2c82076f4ef10
Thomas Silloway <tcsillow@gmail.com>
2023-02-06 02:48:05 -0500

    Fix for delay not being applied because of recursive logic

--- src/generate_tts_for_json_script.py
+++ src/generate_tts_for_json_script.py
@@ -9,6 +9,9 @@ def generate_next_file(filename):
     if len(data) == 0:
         return
 
+    # This delay is necessary so we don't hit request limits for Azure
+    time.sleep(2.5)
+
     item = data.pop(0)
     text = item['text']
     sound = item['sound']
@@ -16,8 +19,6 @@ def generate_next_file(filename):
     # call the generate_file_for_text function and pass the text and sound value as old_json_file
     tts_generator = TTSFileGenerator(config)
     tts_generator.generate_file_for_text(text, generate_next_file, filename=sound)
-    # This delay is necessary so we don't hit request limits for Azure
-    time.sleep(2.5)
 
 
 config = Config('test')
